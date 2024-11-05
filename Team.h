c1324b0d3e9d311675f6b5c289c2a0ea6d83c015
munbeam13 <allayarov13a@gmail.com>
2024-11-05 18:09:25 +0300

    BestFit
    Сделал алгоритм BestFit для решения поставленной задачи. Переделал под входные и выходные значения и провел тесты. Все работает.

--- Team.h
+++ Team.h
@@ -6,7 +6,8 @@ using method = std::unordered_set<int>(*)
 (const std::vector<int>&, const std::vector<int>&);
 
 //ќбъ€вл€ете здесь свою функцию как по примеру
-std::unordered_set<int> simplestPackagingGrigorev(const std::vector<int>& objects,
-	const std::vector<int>& containers);
+std::unordered_set<int> simplestPackagingGrigorev(const std::vector<int>& objects, const std::vector<int>& containers);
 
 std::unordered_set<int> AntSolutionLeontev(const std::vector<int>& objects, const std::vector<int>& containers);
+
+std::unordered_set<int> bestFitAllayarov(const std::vector<int>& objects, const std::vector<int>& containers);
