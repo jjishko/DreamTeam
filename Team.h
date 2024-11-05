f33fe4432ecb5f82d743b275657ec9e2c21f4457
Jishko <gvv-2005@yandex.ru>
2024-11-05 19:42:14 +0300

    Исправление проблем с кодировкой
    Искренне надеюсь, что только один раз

--- Team.h
+++ Team.h
@@ -6,7 +6,7 @@
 using method = std::unordered_set<int>(*)
 (const std::vector<int>&, const std::vector<int>&);
 
-//Îáúÿâëÿåòå çäåñü ñâîþ ôóíêöèþ êàê ïî ïðèìåðó
+//Объявляете здесь свою функцию как по примеру
 std::unordered_set<int> simplestPackagingGrigorev(const std::vector<int>& objects, const std::vector<int>& containers);
 std::unordered_set<int> AntSolutionLeontev(const std::vector<int>& objects, const std::vector<int>& containers);
 std::unordered_set<int> TabuSearchOsadchenko(const std::vector<int>& weights, const std::vector<int>& container);
